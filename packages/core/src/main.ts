0e9faa764c2fc4d8049b5e0d78bbd4c28819a090
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2025-09-06 16:11:09 +0100

    chore: set up prettier

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import s from 'crypto'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
