7fe80b921bd750319f76e1f7738b103a6b4c4eb0
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-01-04 12:24:58 +0100

    fix(core): add missing export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,3 @@
 export { hueyColor } from './core'
-export type { ColorFormat, HSLA, HUEY_COLOR, HueyColor, HueyColorSymbol, HueyError, HueyOptions, OKLCH, RGBA } from './types'
+export type * from './types'
 export * from './utils/index'
