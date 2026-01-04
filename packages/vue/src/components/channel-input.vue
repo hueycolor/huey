60e61736b8e2058de4dbb71cc11cf588d05252f8
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-01-04 12:27:00 +0100

    refactor(vue): channel-input max/min

--- packages/vue/src/components/channel-input.vue
+++ packages/vue/src/components/channel-input.vue
@@ -1,33 +1,17 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import type { ColorChannel } from '@huey/core'
+import { getChannelBounds } from '@huey/core'
 
 const { channel } = defineProps<ChannelInputProps>()
 
 const modelValue = defineModel<number>()
 
-const max = computed(() => {
-  if (['r', 'g', 'b'].includes(channel))
-    return 255
-
-  if (channel === 'h')
-    return 360
-
-  if (['s', 'l', 'c'].includes(channel))
-    return 100
-
-  if (channel === 'a')
-    return 100
-
-  return 100
-})
+const { min, max } = getChannelBounds(channel)
 </script>
 
 <script lang="ts">
 export interface ChannelInputProps {
-  channel: 'r' | 'g' | 'b'
-    | 'h' | 's' | 'l'
-    | 'l' | 'c' | 'h'
-    | 'a'
+  channel: ColorChannel
 }
 </script>
 
@@ -36,12 +20,8 @@ export interface ChannelInputProps {
     v-model="modelValue"
     type="number"
     inputmode="numeric"
-    :min="0"
+    :min="min"
     :max="max"
     :aria-label="`color channel (${channel}) input`"
   >
 </template>
-
-<style>
-
-</style>
