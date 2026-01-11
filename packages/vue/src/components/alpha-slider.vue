53aa888ca99ed78492dd8a531046b2061689a34e
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-01-11 20:25:57 +0100

    chore(vue): improve alpha slider tiled bg

--- packages/vue/src/components/alpha-slider.vue
+++ packages/vue/src/components/alpha-slider.vue
@@ -9,13 +9,7 @@ const { hue, saturation, lightness, alpha } = useHueyContext()
 
 const alphaSliderBg = computed(() => {
   return `
-    linear-gradient(to right, hsla(${hue.value}, ${saturation.value}%, ${lightness.value}%, 0), hsla(${hue.value}, ${saturation.value}%, ${lightness.value}%, 1)),
-      repeating-conic-gradient(
-        #ffffff 0deg,
-        #ffffff 90deg,
-        rgba(0, 0, 0, 0.3) 90deg,
-        rgba(0, 0, 0, 0.3) 180deg
-      ) 0% -25% / 6px 6px
+  linear-gradient(90deg, hsla(${hue.value}, ${saturation.value}%, ${lightness.value}%, 0) 0 calc(var(--huey-slider-track-height) / 2), hsla(${hue.value}, ${saturation.value}%, ${lightness.value}%, 1) calc(100% - calc(var(--huey-slider-track-height) / 2)) 100%), url('data:image/svg+xml;utf8,%3Csvg%20width%3D%222%22%20height%3D%222%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%3Cpath%20d%3D%22M0%200h1v2h1V1H0%22%20fill-rule%3D%22nonzero%22%20fill%3D%22%23e1e1e1%22/%3E%3C/svg%3E') 0 0 / auto 66.67%, #fff
   `
 })
 
@@ -36,6 +30,7 @@ export interface AlphaSliderProps {}
     :min="0"
     :step="0.01"
     :style="{
+
       'background': alphaSliderBg,
       '--huey-thumb-color': thumbBg,
     }"
