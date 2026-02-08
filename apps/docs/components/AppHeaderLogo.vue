3a9d0cf223b098cc6764ed5a54ec6488b4430370
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-02-08 14:14:08 +0100

    chore: add tokens to docs app

---
+++ apps/docs/components/AppHeaderLogo.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <span huey-wordmark>Huey</span>
+</template>
+
+<style scoped>
+[huey-wordmark] {
+  font: var(--title-h4);
+  height: auto;
+}
+</style>
--- apps/docs/nuxt.config.ts
+++ apps/docs/nuxt.config.ts
@@ -3,6 +3,20 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
   ],
+  css: ['~/assets/global.css'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preload',
+          as: 'font',
+          type: 'font/woff',
+          href: '/fonts/Cubano.woff',
+          crossorigin: 'anonymous',
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
 })
