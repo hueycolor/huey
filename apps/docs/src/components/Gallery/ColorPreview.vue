ed585ee1c9c15c66fdf3a5a6ce50586d61994405
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-03-10 23:18:10 +0000

    chore: add color preview to galleru

---
+++ apps/docs/src/components/Gallery/ColorPreview.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { hueyColor } from '@hueycolor/core'
+import { ColorPreview, HueyRoot } from '@hueycolor/vue'
+import { ref } from 'vue'
+
+const color = ref(hueyColor('#ff000080'))
+</script>
+
+<template>
+  <div class="hex-input">
+    <HueyRoot v-model="color">
+      <ColorPreview />
+    </HueyRoot>
+  </div>
+</template>
+
+<style scoped>
+:deep([huey-preview]) {
+  --preview-tile: 64px
+}
+
+.hex-input {
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-16);
+}
+</style>
