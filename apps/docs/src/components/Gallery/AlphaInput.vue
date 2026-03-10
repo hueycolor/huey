f0a2fc257fa35d3f89b91a518f321c7d0e2bc7d0
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-03-10 23:06:34 +0000

    chore: add alpha input to gallery

---
+++ apps/docs/src/components/Gallery/AlphaInput.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { hueyColor } from '@hueycolor/core'
+import { AlphaInput, HueyRoot } from '@hueycolor/vue'
+import { ref } from 'vue'
+
+const color = ref(hueyColor('#ff0000'))
+</script>
+
+<template>
+  <div class="hex-input">
+    <HueyRoot v-model="color">
+      <AlphaInput />
+    </HueyRoot>
+  </div>
+</template>
+
+<style scoped>
+:deep([huey-input]) {
+  border: 1px solid var(--color-neutral-100);
+  border-radius: var(--radius-8);
+  height: 48px;
+  padding-inline: var(--spacing-8);
+}
+
+.hex-input {
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-16);
+}
+</style>
