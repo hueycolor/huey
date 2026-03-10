c63a6faaf2c6be796171d2f65555c17903e7d692
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-03-10 21:08:18 +0000

    chore: add saturation area to gallery

---
+++ apps/docs/src/components/Gallery/SaturationArea.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { hueyColor } from '@hueycolor/core'
+import { HueyRoot, SaturationArea } from '@hueycolor/vue'
+import { ref } from 'vue'
+
+const color = ref(hueyColor('#ff0000'))
+</script>
+
+<template>
+  <div class="vue-component">
+    <HueyRoot v-model="color">
+      <SaturationArea />
+    </HueyRoot>
+  </div>
+</template>
+
+<style scoped>
+:deep([huey-area]) {
+  height: 200px;
+  width: 200px;
+}
+</style>
