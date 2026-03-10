f68eac17d8b037c9334e42e353a3e090f95c5e45
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-03-10 22:00:22 +0000

    chore: add alpha slider to gallery

---
+++ apps/docs/src/components/Gallery/AlphaSlider.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { hueyColor } from '@hueycolor/core'
+import { AlphaSlider, HueyRoot } from '@hueycolor/vue'
+import { ref } from 'vue'
+
+const color = ref(hueyColor('#ff0000'))
+</script>
+
+<template>
+  <div class="hsl-sliders">
+    <HueyRoot v-model="color">
+      <AlphaSlider />
+    </HueyRoot>
+  </div>
+</template>
+
+<style scoped>
+:deep([huey-slider-track]) {
+  --huey-slider-track-width: 100%;
+}
+
+.hsl-sliders {
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-16);
+}
+</style>
