8ba81cec482e7c1a284be8c53afbd645b770562f
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-03-10 23:16:41 +0000

    chore: add swatch to gallery

---
+++ apps/docs/src/components/Gallery/ColorSwatch.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { hueyColor } from '@hueycolor/core'
+import { ColorSwatch, HueyRoot } from '@hueycolor/vue'
+import { ref } from 'vue'
+
+const color = ref(hueyColor('#ff0000'))
+</script>
+
+<template>
+  <div class="hex-input">
+    <HueyRoot v-model="color">
+      <ColorSwatch :swatch="['#007680', '#24B2FF', '#FFBA51', '#FFD013', '#F65600']" />
+    </HueyRoot>
+  </div>
+</template>
+
+<style scoped>
+:deep([huey-swatch]) {
+  background-color: white;
+  padding: var(--spacing-8);
+  border-radius: var(--radius-8);
+}
+
+.hex-input {
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-16);
+}
+</style>
