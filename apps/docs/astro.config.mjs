f039f051aa7ef545cfaeb1ada031136bc84d0013
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-04-13 19:56:49 +0100

    chore: undo last change

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -6,9 +6,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    assets: 'assets',
-  },
   integrations: [starlight({
     title: 'Huey',
     expressiveCode: {
