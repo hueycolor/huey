b39fe61df785dd8dc2ce8df821fc1935a9ce249b
Eihab Khan <143792300+eihabkhan@users.noreply.github.com>
2026-04-19 01:11:57 +0100

    fix(docs): color dropper

--- apps/docs/src/components/gallery/ColorDropper.vue
+++ apps/docs/src/components/gallery/ColorDropper.vue
@@ -4,16 +4,18 @@ import { ColorDropper, HueyRoot } from '@hueycolor/vue'
 import { ref } from 'vue'
 
 const color = ref(hueyColor('#1A3263'))
+// @ts-expect-error TS unaware of EyeDropper
+const isSupported = typeof window !== 'undefined' && !!window.EyeDropper
 </script>
 
 <template>
   <div class="color-dropper">
-    <HueyRoot v-model="color">
+    <HueyRoot v-if="isSupported" v-model="color">
       <ColorDropper>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pipette-icon lucide-pipette"><path d="m12 9-8.414 8.414A2 2 0 0 0 3 18.828v1.344a2 2 0 0 1-.586 1.414A2 2 0 0 1 3.828 21h1.344a2 2 0 0 0 1.414-.586L15 12" /><path d="m18 9 .4.4a1 1 0 1 1-3 3l-3.8-3.8a1 1 0 1 1 3-3l.4.4 3.4-3.4a1 1 0 1 1 3 3z" /><path d="m2 22 .414-.414" /></svg>
       </ColorDropper>
     </HueyRoot>
-    <div class="fallback">
+    <div v-else class="fallback">
       <span>(◡︵◡)</span>
       <span>
         EyeDropper API not supported
@@ -23,15 +25,6 @@ const color = ref(hueyColor('#1A3263'))
 </template>
 
 <style scoped>
-:deep([huey-color-dropper]) {
-  background-color: white;
-  height: 48px;
-  width: 48px;
-  border-radius: var(--radius-8);
-  border: 1px solid var(--color-neutral-100);
-  transition: all 300ms ease-out;
-}
-
 .color-dropper {
   display: flex;
   flex-direction: column;
@@ -39,7 +32,7 @@ const color = ref(hueyColor('#1A3263'))
   gap: var(--spacing-16);
 
   .fallback {
-    display: none;
+    display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
@@ -48,12 +41,6 @@ const color = ref(hueyColor('#1A3263'))
     font: var(--docs-sm);
     color: var(--color-neutral-500);
     text-wrap: pretty;
-  }
-
-  &:not(:has([huey-color-dropper])) {
-    .fallback {
-      display: flex;
-    }
   }
 }
 </style>
